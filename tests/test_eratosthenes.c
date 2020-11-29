b211ec33b1784b218b20eba24330905242b77c14
Ammar Faizi <ammarfaizi2@gmail.com>
2020-11-29 10:10:31 +0700

    Fix eratosthenes algorithm
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- tests/test_eratosthenes.c
+++ tests/test_eratosthenes.c
@@ -5,8 +5,24 @@
 extern uint64_t *
 eratosthenes(uint64_t n, uint64_t *m);
 
+extern void myFree(void *ptr);
+
 int
 main()
 {
-  eratosthenes(100, NULL);
+  uint64_t n = 121;
+  uint64_t num;
+  uint64_t *arr = eratosthenes(n, &num);
+
+  printf("Number of prime numbers between 1 and %ld = %d\n", n, num);
+  for (uint16_t i = 0; i < num; i++) {
+    printf("%*ld ", 5, arr[i]);
+    if ((i % 4) == 0) {
+      printf("\n");
+    }
+  }
+
+  printf("\n");
+
+  myFree(arr);
 }
