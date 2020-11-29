aca64265342314b4ddd2b787188b977897410dbd
Ammar Faizi <ammarfaizi2@gmail.com>
2020-11-29 11:15:56 +0700

    Update nlog calculation in eratosthenes.S
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- tests/test_eratosthenes.c
+++ tests/test_eratosthenes.c
@@ -7,24 +7,30 @@ eratosthenes(uint64_t n, uint64_t *m);
 
 extern void myFree(void *ptr);
 
+#define MY_TEST(N)                                                      \
+do {                                                                    \
+  uint64_t n = (N);                                                     \
+  uint64_t num;                                                         \
+  uint64_t *arr = eratosthenes(n, &num);                                \
+                                                                        \
+  printf("There are %ld prime numbers between 1 and %ld\n", num, n);    \
+                                                                        \
+  for (uint64_t i = 0; i < num; i++) {                                  \
+    if ((i % 6) == 0) {                                                 \
+      printf("\n");                                                     \
+    }                                                                   \
+    printf("%*ld ", 5, arr[i]);                                         \
+  }                                                                     \
+  printf("\n\n");                                                       \
+  myFree(arr);                                                          \
+} while (0)
+
+
 int
 main()
 {
-  uint64_t n = 1000;
-  uint64_t num;
-  uint64_t *arr = eratosthenes(n, &num);
-
-  printf("There are %ld prime numbers between 1 and %ld\n", num, n);
-
-  for (uint64_t i = 0; i < num; i++) {
-    if ((i % 6) == 0) {
-      printf("\n");
-    }
-
-    printf("%*ld ", 5, arr[i]);
-  }
-
-  printf("\n");
-
-  myFree(arr);
+  MY_TEST(5);
+  MY_TEST(10);
+  MY_TEST(100);
+  MY_TEST(1000);
 }
